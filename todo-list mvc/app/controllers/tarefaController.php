3c5a589c067b9f1edc9795a4c0fa82507767ecd9
instantes <fernandes.maria1@escolar.ifrn.edu.br>
2025-10-14 08:24:40 -0300

    subindo

--- todo-list mvc/app/controllers/tarefaController.php
+++ todo-list mvc/app/controllers/tarefaController.php
@@ -1,22 +1,30 @@
-<?php
+<?php 
+    require_once __DIR__ . '/../models/tarefa.php';
 
-require_once __DIR__ . '/../models/tarefa.php';
+    class TarefaController {
+        private $tarefaModel;
 
-class TarefaController{
-    private $tarefaModel;
-
-    public function __construct(){
-        $this->tarefaModel = new Tarefa();
-    }
-
-    ## criar
-
-    public function criar () {
-        if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
-            $this->tarefaModel->criar($_POST('descricao'));
+        public function __construct() {
+            $this->tarefaModel = new Tarefa();
         }
-        header(header: "location: index.php");
+
+        public function index() {
+            $tarefas = $this->tarefaModel->listar();
+            include __DIR__ .'/../views/listar.php';
+    }        
+
+        public function criar() {
+            if (isset($_POST['descricao']) && !empty(($_POST['descricao']))) {
+                $this->tarefaModel->criar($_POST['descricao']);
+            }
+            header('location: index.php');
     }
 
+    public function excluir() {
+        if (isset($_GET['delete'])) {
+            $this->tarefaModel->excluir($_GET['delete']);
+        }
+        header('location: index.php');
+    }
 }
 ?>
